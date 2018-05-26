1078b620b82fea92c8e370399ff5fbb6b5a75e99
Lenovo servando <servandotaibo@gmail.com>
2018-05-26 13:43:24 +0200

    Whole game.

---
+++ Assets/Scripts/PhysicsObject.cs
@@ -0,0 +1,99 @@
+﻿using UnityEngine;
+
+public class PhysicsObject : MonoBehaviour
+{
+    public const float MIN_COLLISION_NORMAL_Y = 0.65f;
+    public const float MIN_MOVE_DISTANCE = 0.001f;
+    public const float SHELL_RADIUS = 0.01f;
+
+    public float m_gravityModifier = 1f;
+
+    protected Rigidbody2D m_rigidBody;
+    protected Vector2 m_velocity;
+    public Vector2 m_targetVelocity;
+    protected ContactFilter2D m_contactFilter;
+    protected RaycastHit2D[] m_collisionHits = new RaycastHit2D[16];
+
+    protected bool m_isGrounded = false;
+    private Vector2 m_moveAlongGround;
+
+    private void OnEnable()
+    {
+        m_rigidBody = GetComponent<Rigidbody2D>();    
+    }
+
+    private void Start()
+    {
+        m_contactFilter.useTriggers = false;
+        m_contactFilter.useLayerMask = true;
+        m_contactFilter.SetLayerMask(Physics2D.GetLayerCollisionMask(gameObject.layer));
+    }
+
+    private void Update()
+    {
+        m_targetVelocity = Vector2.zero;
+        CalculateMovement();    
+    }
+
+    protected virtual void CalculateMovement()
+    {    
+    }
+
+    void FixedUpdate ()
+    {
+        m_isGrounded = false;
+        m_velocity += m_gravityModifier * Physics2D.gravity * Time.deltaTime;
+        m_velocity.x = m_targetVelocity.x;
+
+        Vector2 delta = m_velocity * Time.deltaTime;
+
+        Vector2 move = m_moveAlongGround * delta.x;
+        Move(move, false);
+
+        move = Vector2.up * delta.y;
+        Move(move, true);        
+	}
+
+    private void Move(Vector2 move, bool isYMove)
+    {
+        float distance = move.magnitude;
+
+        // To prevent this physics object from being applied physics when it's static
+        if (distance > MIN_MOVE_DISTANCE)
+        {
+            int count = m_rigidBody.Cast(move, m_contactFilter, m_collisionHits, distance + SHELL_RADIUS);
+
+            for (int i = 0; i < count; i++)
+            {
+                Vector2 collisionNormal = m_collisionHits[i].normal;
+
+                // Makes sure the character can stand on this collision
+                if (collisionNormal.y > MIN_COLLISION_NORMAL_Y)
+                {
+                    m_isGrounded = true;
+
+                    if (isYMove)
+                    {
+                        m_moveAlongGround = new Vector2(collisionNormal.y, -collisionNormal.x);
+                        collisionNormal.x = 0;
+                    }
+                }
+
+                // Checks if we need to correct velocity to prevent the character from going inside the collider
+                float projection = Vector2.Dot(m_velocity, collisionNormal);
+                if (projection < 0)
+                {
+                    m_velocity = m_velocity - projection * collisionNormal;
+                }
+
+                float modifiedDistance = m_collisionHits[i].distance - SHELL_RADIUS;
+                if (modifiedDistance < distance)
+                {
+                    distance = modifiedDistance;
+                }
+            }
+
+            m_rigidBody.position += move.normalized * distance;
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+public class PlayerController : PhysicsObject
+{
+    public float m_horizontalSpeed = 7f;
+    public float m_jumpSpeed = 7f;
+
+    private SpriteRenderer m_spriteRenderer;
+    private Animator m_animator;
+
+    private void Awake()
+    {
+        m_spriteRenderer = GetComponent<SpriteRenderer>();
+        m_animator = GetComponent<Animator>();
+    }
+
+    protected override void CalculateMovement()
+    {
+        Vector2 move = Vector2.zero;
+        move.x = Input.GetAxis("Horizontal") * m_horizontalSpeed;
+
+        if (Input.GetButtonDown("Jump") && m_isGrounded)
+        {
+            m_velocity.y = m_jumpSpeed;
+        }
+        else if (Input.GetButtonUp("Jump"))
+        {
+            if (m_velocity.y > 0f)
+            {
+                m_velocity *= 0.5f;
+            }
+        }
+
+        bool flipX = m_spriteRenderer.flipX;        
+        if ((move.x > 0.001f && flipX) ||
+            (move.x < -0.001f && !flipX))
+        {
+            m_spriteRenderer.flipX = !m_spriteRenderer.flipX;
+        }
+
+        m_animator.SetBool("grounded", m_isGrounded);
+        m_animator.SetFloat("velocityX", Mathf.Abs(move.x) / m_horizontalSpeed);
+
+        m_targetVelocity = move;
+    }
+}
